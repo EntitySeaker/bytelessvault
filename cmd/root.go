ed4edd1771ab43c5f0a76068dfe9d29fe9298b2c
Stowic <stowic@citizen-zero.xyz>
2023-09-24 10:22:46 -0600

    Added basic functionality.

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,24 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"bytes"
+	"crypto/sha512"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
-
+var char_list string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_\"#$%&'*+,./:;=?!@\\^`|~[]{}()<>"
+var length string
+var passphrase string
+var alias string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,7 +32,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print(calcPass(passphrase, alias, length))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +55,30 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&length, "length", "l", "32", "Length of password")
+	rootCmd.Flags().StringVarP(&passphrase, "passphrase", "p", " ", "Pass Phrase to unlock passwords")
+	rootCmd.MarkFlagRequired("passphrase")
+	rootCmd.Flags().StringVarP(&alias, "alias", "a", " ", "Pass Alias to unlock passwords")
+	rootCmd.MarkFlagRequired("alias")
 }
 
+func calcPass(seedPhrase string, passAlias string, length string) string {
+	hash := sha512.Sum512([]byte(seedPhrase + passAlias + length))
+	var userPass string
+	// Convert the hash bytes to an integer.
+	var seed1 int64
+	binary.Read(bytes.NewReader(hash[:]), binary.BigEndian, &seed1)
+	rand.Seed(seed1)
 
+	input_length_int, err := strconv.Atoi(length)
+	if err != nil {
+		fmt.Println("Error: not a number!: ", err)
+		os.Exit(1)
+	}
+
+	for i := 0; i < input_length_int; i++ {
+		//fmt.Printf("%c", char_list[rand.Intn(len(char_list))])
+		userPass = userPass + string(char_list[rand.Intn(len(char_list))])
+	}
+	return userPass
+}
