8f3fad4ea49af653191526b424ca4cc521b0a9f1
Stowic <stowic@citizen-zero.xyz>
2023-09-24 10:26:29 -0600

    Added basic command description.

--- cmd/root.go
+++ cmd/root.go
@@ -23,13 +23,8 @@ var alias string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bytevault",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Mathimagically Stores Passwords.",
+	Long:  `Mathimagically Stores Passwords..`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
